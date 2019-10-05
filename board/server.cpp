8c28143cf9550389cb38c2b7c34bd56bd37c968a
Carloto <viniciuscarloto@gmail.com>
2019-10-05 17:01:39 -0300

    started board server

---
+++ board/server.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "addsub.h"
+#include <iostream>
+#include <list>
+#include <iterator>
+
+/* implementalção da função add */
+int *add_1_svc(operands *argp, struct svc_req *rqstp)
+{
+   static int result;
+
+   printf("Recebi chamado: add %d %d\n", argp->x, argp->y);
+   result = argp->x + argp->y;
+   return (&result);
+}
+
+/* implementação da função sub */
+int *sub_1_svc(operands *argp, struct svc_req *rqstp)
+{
+   static int result;
+
+   printf("Recebi chamado: sub %d %d\n", argp->x, argp->y);
+   result = argp->x - argp->y;
+   return (&result);
+}
